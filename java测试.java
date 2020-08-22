0355a9fdfedaedb4e8a7a28a415035c95541928d
shj <1460493137@qq.com>
2020-08-22 11:05:21 +0800

    测试22

--- java测试.java
+++ java测试.java
@@ -1,6 +1,7 @@
 public class Demo{
 	public static void main(String[] args){
-		System.out.print("demo1demo02");
-		System.out.print("demo1demo02");
+		System.out.print("demo1demo0111");
+		System.out.print("demo1demo0111");
+		System.out.print("demo1demo0111");
 	}
 }
