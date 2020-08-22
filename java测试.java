3433ad6e548401145a8c365d7b08ab70832e1995
shj <1460493137@qq.com>
2020-08-22 11:24:25 +0800

    cs

--- java测试.java
+++ java测试.java
@@ -1,7 +1,14 @@
 public class Demo{
 	public static void main(String[] args){
-		System.out.print("demo1demo0111");
-		System.out.print("demo1demo0111");
-		System.out.print("demo1demo0111");
+		System.out.print("demo1111");
+		System.out.print("demo1demo0222");
+		System.out.print("dddddemo0111");
+	}
+}
+public class Ddd{
+	public static void main(String[] args){
+		System.out.print("dqqq");
+		System.out.print("demo1ssss");
+		System.out.print("demo1fff1");
 	}
 }
